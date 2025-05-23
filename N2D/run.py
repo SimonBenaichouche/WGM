1c05f030067af55e9d0a5442a385802941c0692a
SimonBenaichouche <39214118+SimonBenaichouche@users.noreply.github.com>
2025-05-23 12:11:50 +0200

    Add files via upload

--- N2D/run.py
+++ N2D/run.py
@@ -20,7 +20,8 @@ from torch.utils.data import TensorDataset, DataLoader, random_split
 
 dataset = TensorDataset(pooled_vorticity, future_states)
 
-# Diviser le dataset en jeux d'entraînement, validation et test
+# split dataset into train val and test
+
 train_size = int(0.9 * len(dataset))
 val_size = int(0.05 * len(dataset))
 test_size = len(dataset) - train_size - val_size
@@ -33,15 +34,11 @@ width, height = 128, 128
 # Création du masque initial
 mask = torch.zeros(height, width)
 
-# Remplissage des blocs de 4 colonnes avec une colonne entière égale à 1 de façon aléatoire
+# create a mask to generate a partial observation setting 
 for block_col_start in range(0, width, 4):
     random_col = block_col_start + np.random.randint(0, 4)
     mask[:, random_col] = 1
 
-# Ajout des bandes horizontales et verticales de 8 pixels de large tous les 32 pixels
-'''for i in range(0, height, 32):
-    mask[i:i+3, :] = 0  # Bandes horizontales
-    mask[:, i-3:i+3] = 0  # Bandes verticales'''
 
 
 '''
@@ -58,20 +55,20 @@ import pytorch_lightning as pl
 try:
     #model.load_state_dict(torch.load("surrogate_step=1"))
     model.load_state_dict(torch.load("weights_last_iter_unettanh"))
-    print("Poids du modèle chargés avec succès.")
+    print("model weight loaded")
 except :
-    print("Échec du chargement des poids du modèle. Utilisation des poids par défaut.")
+    print("error")
     
-# Créer le DataLoader pour l'entraînement et la validation
+
 train_data_loader = data.DataLoader(train_dataset, batch_size=10, shuffle=True, drop_last=True, num_workers=0)
 val_data_loader = data.DataLoader(val_dataset, batch_size=40, shuffle=False, drop_last=False, num_workers=0)
-# Configurer le Trainer avec multi-GPU si disponible
+# configure the trainer
 trainer = pl.Trainer(
-    max_epochs=5,  # Nombre d'epochs
+    max_epochs=10,  # numbers of epoch
     accelerator='gpu',
-    devices=1,  # Utilise tous les GPUs disponibles,# Utilise "dp" si plusieurs GPUs, sinon "auto"
+    devices=1,  
     check_val_every_n_epoch=1, 
-    accumulate_grad_batches=1  # Validation après chaque epoch
+    accumulate_grad_batches=1  
 )
 
 # Entraîner le modèle et lancer la validation
